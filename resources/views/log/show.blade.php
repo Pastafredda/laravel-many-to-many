608fc64586e668229fdd23e4efbc19e4c66e514f
Pastafredda <justspotify09@gmail.com>
2023-08-02 17:18:47 +0200

    stampa dei ruoli

--- resources/views/log/show.blade.php
+++ resources/views/log/show.blade.php
@@ -9,6 +9,12 @@
         <li>editore:{{ $book->editor }}</li>
         <li>genere:{{ $book->type->name }}</li>
         <li>disponibilità:{{ $book->is_available ? 'si' : 'no' }} </li>
+    </ul>
 
+    <h4>materiale utilizzato: {{ count($book->technologies) }}</h4>
+    <ul>
+        @foreach ($book->technologies as $technology)
+            <li>{{ $technology->name }}</li>
+        @endforeach
     </ul>
 @endsection
