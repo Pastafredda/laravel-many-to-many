88f06bc5687f3ffb9d05fa47a66a97ed36d2c0fa
Pastafredda <justspotify09@gmail.com>
2023-08-19 17:59:55 +0200

    risolto is_available

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -23,20 +23,9 @@ class LogController extends Controller
     public function store(Request $request){
         $data= $request ->all();
 
+
         $book  = Book :: create($data);
-        $request->validate([
-            'is_available' => 'required|in:si,no',
-            // Altre regole di validazione se necessario
-        ]);
 
-        // Crea una nuova istanza del modello Product
-        $product = new Product();
-
-        // Imposta il valore dell'opzione di disponibilità nel modello in base a ciò che è stato selezionato nel modulo
-        $product->is_available = $request->input('is_available');
-
-        // Salva il modello nel database
-        $product->save();
         return redirect()-> route('log.show', $book -> id);
     }
 }
